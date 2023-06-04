bb21d3faf9655e7cf6757e46060e8a85740ced64
seongwoo <seongwoo0513@uos.ac.kr>
2023-06-04 15:03:48 +0900

    feat : init setting #PAR-38

---
+++ src/main/java/online/partyrun/partyrunauthorizationmodule/PartyRunAuthorizationModuleApplication.java
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunauthorizationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyRunAuthorizationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyRunAuthorizationModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/partyrun/partyrunauthorizationmodule/PartyRunAuthorizationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package online.partyrun.partyrunauthorizationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyRunAuthorizationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
